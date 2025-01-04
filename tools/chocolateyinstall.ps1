2e02ff884824201e5411fe47f555aef098c6cff0
Sebastian huus <54155988+sebastianhuus@users.noreply.github.com>
2025-01-04 21:49:20 +0100

    revert e431b55 since checksum is mandatory for scripts installing remote executables on the chocolatey community repo

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -6,6 +6,8 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'EXE'
   url           = $url
+  checksum      = '07292508E8BFE72D661732B87DAE4B9B'
+  checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
   silentArgs   = '/S'
 }
 
