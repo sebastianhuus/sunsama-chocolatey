4f0a0585d6b1aec7759fe861b3310a4051767c0f
Sebastian huus <54155988+sebastianhuus@users.noreply.github.com>
2025-02-13 09:58:56 +0100

    Upgrade to version 3.0.7

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'EXE'
   url           = $url
-  checksum      = '07292508E8BFE72D661732B87DAE4B9B'
+  checksum      = 'A0FEB53CB11987EA692AE5EBC8A1147F466E4B9F66233CFF2C2D370A6327EBDE'
   checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
   silentArgs   = '/S'
 }
